f2e425154e7a244fec6ee1da3b51f2ba6c63c332
igunnuoj <igunnuoj@gmail.com>
2023-03-10 13:59:24 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,31 +1,14 @@
-#include <stdio.h>
-
-#include "main.h"
-
-
+#include <unistd.h>
 
 /**
-
- * main - prints the name of the program
-
- * @argc: number of arguments
-
- * @argv: array of arguments
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(int argc __attribute__((unused)), char *argv[])
-
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
 {
-
-	printf("%s\n", *argv);
-
-
-
-	return (0);
-
+return (write(1, &c, 1));
 }
+
